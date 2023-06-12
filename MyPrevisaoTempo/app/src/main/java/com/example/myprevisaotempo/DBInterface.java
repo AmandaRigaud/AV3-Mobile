107c85e45886853ade8f70e20f7a8216ef1e7dca
Pototo-exe <touchemachado@gmail.com>
2023-06-12 03:55:38 -0300

    [Feat] Leitura do banco e Criação do ListView no Historico
    Acho que meu git deu problema e trouxe parte do front pra essa branch, Mas usar a classe histórico ja criada faz sentido

--- MyPrevisaoTempo/app/src/main/java/com/example/myprevisaotempo/DBInterface.java
+++ MyPrevisaoTempo/app/src/main/java/com/example/myprevisaotempo/DBInterface.java
@@ -2,33 +2,82 @@ package com.example.myprevisaotempo;
 
 
         import android.content.Context;
+        import android.database.Cursor;
         import android.database.sqlite.SQLiteDatabase;
         import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBInterface {
-    public SQLiteDatabase TempoDB;
+        import java.lang.reflect.Array;
+        import java.util.ArrayList;
+        import java.util.Collections;
 
-    public void criarBanco(){
-        try{
-            TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo", null );
+public class DBInterface {
+    public static SQLiteDatabase TempoDB;
+
+    public void criarBanco() {
+        try {
+            TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo", null);
             TempoDB.execSQL("CREATE TABLE IF NOT EXISTS Historico( " +
                     "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "Cidade VARCHAR," +
                     "Latitude VARCHAR," +
                     "Longitude VARCHAR)");
             TempoDB.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void InserirHistorico(String Latitude, String Longitude, String Cidade){
-        TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo",  null );
-        String Add = "Insert into Historico (Cidade, Latitude, Longitude) VALUES ("+Cidade+","+Latitude+","+Longitude+")";
+
+    public void InserirHistorico(String Latitude, String Longitude, String Cidade) {
+        TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo", null);
+        String Add = "Insert into Historico (Cidade, Latitude, Longitude) VALUES (" + Cidade + "," + Latitude + "," + Longitude + ")";
         TempoDB.execSQL(Add);
     }
-    public void InserirHistorico(String Latitude, String Longitude){
-        TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo",  null );
-        String Add = "Insert into Historico (Cidade, Latitude, Longitude) VALUES ("+"Not Specified"+","+Latitude+","+Longitude+")";
+
+    public void InserirHistorico(String Latitude, String Longitude) {
+        TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo", null);
+        String Add = "Insert into Historico (Cidade, Latitude, Longitude) VALUES (" + "Not Specified" + "," + Latitude + "," + Longitude + ")";
         TempoDB.execSQL(Add);
+    }
+
+    public static ArrayList<SaidaDB> ReturnList() {
+        int id;
+        String Cidade;
+        String Latitude;
+        String Longitude;
+    try {
+        TempoDB = SQLiteDatabase.openOrCreateDatabase("Myprevisaotempo", null);
+        Cursor C = TempoDB.rawQuery("SELECT id, Cidade, Latitude, Longitude FROM Historico", null);
+        ArrayList<SaidaDB> Result = new ArrayList<SaidaDB>();
+
+        C.moveToFirst();
+        while (C != null){
+            id = C.getInt(C.getColumnIndexOrThrow("id"));
+            Cidade = C.getString(C.getColumnIndexOrThrow("Cidade"));
+            Latitude = C.getString(C.getColumnIndexOrThrow("Latitude"));
+            Longitude = C.getString(C.getColumnIndexOrThrow("Longitude"));
+            Result.add(new SaidaDB(id,Cidade,Latitude,Longitude));
+        }
+
+        Collections.reverse(Result);
+        TempoDB.close();
+        return Result;
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+    return new ArrayList<SaidaDB>();
+    }
+}
+
+class SaidaDB{
+    public int id;
+    public String Cidade;
+    public String Latitude;
+    public String Longitude;
+
+    public SaidaDB(int id, String Cidade, String Latitude, String Longitude){
+        this.id = id;
+        this.Cidade = Cidade;
+        this.Latitude = Latitude;
+        this.Longitude = Longitude;
     }
 }
---
+++ MyPrevisaoTempo/app/src/main/java/com/example/myprevisaotempo/Historico.java
@@ -0,0 +1,102 @@
+package com.example.myprevisaotempo;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import androidx.core.view.WindowCompat;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myprevisaotempo.databinding.ActivityHistoricoBinding;
+
+import java.util.ArrayList;
+
+public class Historico extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityHistoricoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHistoricoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        ListView LV = findViewById(R.id.listview_first);
+
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_historico);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_historico);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void getHistory(ListView LV) {
+        ArrayList<SaidaDB> ListaDB = DBInterface.ReturnList();
+        DBListAdapter historyAdapter = new DBListAdapter(this, ListaDB);
+        LV.setAdapter(historyAdapter);
+        LV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SaidaDB Item = ListaDB.get(position);
+                System.out.println("Clicado: " + Item.Latitude + ", " + Item.Longitude + ", " + Item.Cidade);
+                // Todo: Fazer isso retornar para pesquisar com essa latitude e longitude
+            }
+        });
+    }
+
+}
+
+
+class DBListAdapter extends ArrayAdapter<SaidaDB>{
+    private LayoutInflater inflater;
+    public DBListAdapter(Context context, ArrayList<SaidaDB> lista){
+        super(context, 0, lista);
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if(convertView == null){
+            convertView = inflater.inflate(android.R.layout.simple_list_item_1,parent, false);
+        }
+        SaidaDB Item = getItem(position);
+
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText(Item.Cidade+ "("+ Item.Latitude+", "+Item.Longitude+")");
+    return textView;
+    }
+}
