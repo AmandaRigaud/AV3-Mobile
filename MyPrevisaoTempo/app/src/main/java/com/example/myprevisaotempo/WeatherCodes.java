e3e0bc7515d55f2174ba19a85bb9c6d2a90cec40
Pototo-exe <touchemachado@gmail.com>
2023-06-14 05:40:13 -0300

    Fiz o dicionario também pq acho que faz mais sentido

--- MyPrevisaoTempo/app/src/main/java/com/example/myprevisaotempo/WeatherCodes.java
+++ MyPrevisaoTempo/app/src/main/java/com/example/myprevisaotempo/WeatherCodes.java
@@ -3,6 +3,8 @@ package com.example.myprevisaotempo;
 // Só chamar WeatherCodes.Code_X
 //Se precisar de algo mais automatico (Code[0] por exemplo, da pra colocar em um array)
 
+import java.util.HashMap;
+
 public  class WeatherCodes {
     public static final String CODE_0 = "Céu limpo";
     public static final String CODE_1 = "Parcialmente limpo";
@@ -32,5 +34,39 @@ public  class WeatherCodes {
     public static final String CODE_95 = "Trovoada";
     public static final String CODE_96 = "Granizo leve";
     public static final String CODE_99 = "Granizo intenso";
+
+    public static final HashMap<String, String> codeMap;
+
+    static{
+        codeMap = new HashMap<>();
+        codeMap.put("CODE_0", "Céu limpo");
+        codeMap.put("CODE_1", "Parcialmente limpo");
+        codeMap.put("CODE_2", "Parcialmente nublado");
+        codeMap.put("CODE_3", "Nublado");
+        codeMap.put("CODE_45", "Nevoeiro");
+        codeMap.put("CODE_48", "Nevoeiro e neve");
+        codeMap.put("CODE_51", "Garoa leve");
+        codeMap.put("CODE_53", "Garoa moderada");
+        codeMap.put("CODE_55", "Garoa intensa");
+        codeMap.put("CODE_56", "Garoa congelada leve");
+        codeMap.put("CODE_57", "Garoa congelada intensa");
+        codeMap.put("CODE_61", "Chuva leve");
+        codeMap.put("CODE_63", "Chuva moderada");
+        codeMap.put("CODE_65", "Chuva intensa");
+        codeMap.put("CODE_66", "Chuva congelada leve");
+        codeMap.put("CODE_67", "Chuva congelada intensa");
+        codeMap.put("CODE_71", "Neve leve");
+        codeMap.put("CODE_73", "Neve moderada");
+        codeMap.put("CODE_75", "Neve intensa");
+        codeMap.put("CODE_77", "Grãos de neve");
+        codeMap.put("CODE_80", "Pancadas de chuva leves");
+        codeMap.put("CODE_81", "Pancadas de chuva moderadas");
+        codeMap.put("CODE_82", "Pancadas de chuva intensas");
+        codeMap.put("CODE_85", "Aguaceiro de neve leve");
+        codeMap.put("CODE_86", "Aguaceiro de neve intensos");
+        codeMap.put("CODE_95", "Trovoada");
+        codeMap.put("CODE_96", "Granizo leve");
+        codeMap.put("CODE_99", "Granizo intenso");
+    }
 
 }
